c0c0f046b4fa8fad6d50cf762608b65d1b56c0d2
marthaannj <marthaanj9@gmail.com>
2019-05-10 15:29:20 +0530

    i am anj this commit on screen shoot

---
+++ screenshot/src/screenshot/screensh.java
@@ -0,0 +1,40 @@
+package screenshot;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import com.google.common.io.Files;
+
+public class screensh {
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+	System.setProperty("webdriver.chrome.driver","c:\\anj\\chromedriver.exe");
+
+	WebDriver dr=new ChromeDriver();
+	Thread.sleep(2000);
+	
+	dr.get("http://www.google.com/");
+	/*File scr= (((TakesScreenshot) dr).getScreenshotAs(OutputType.FILE));
+	
+Files.copy(scr,new File("d:\\anjgo.png"));*/
+WebElement e=dr.findElement(By.xpath("//input[@name='q']"));
+e.sendKeys("selenium");
+e.sendKeys(Keys.RETURN);
+	/*e.clear();
+	e.click();
+	
+	Thread.sleep(2000);
+	dr.findElement(By.className("erKQq")).click();*/
+	
+	}
+
+}
